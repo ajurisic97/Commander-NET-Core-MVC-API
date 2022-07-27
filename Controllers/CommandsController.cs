ce77f90fa01b4bbc47e88855d61ba5c1b4e227f9
Andelo Jurisic <ajurisic@pmfst.hr>
2022-07-27 13:42:42 +0200

    Error resolved

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -90,7 +90,7 @@ namespace Commander.Controllers
                 return NotFound();
             }
             _repository.DeleteCommand(commandModelFromRepo);
-            _repository.SaveChanges()
+            _repository.SaveChanges();
             return NoContent();
         }
     }
